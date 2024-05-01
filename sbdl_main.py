210d55c0ea7e7f24dff69bdc71b54bdaf1e91db4
kbalasubramaniam <kokhila.balasubramaniam@am.sony.com>
2024-05-01 07:41:59 +0530

    first commit

---
+++ sbdl_main.py
@@ -0,0 +1,16 @@
+import sys
+from lib.logger import Log4j
+from lib import Utils
+
+if __name__=='__main.py__':
+    if(len(sys.argv)<3):
+        print("Usage: spdl{prod,local,dev}{load_date}:Arguments missing")
+        sys.exit(-1)
+
+    job_run_env=sys.argv[1].upper()
+    load_date=sys.argv[2]
+
+    spark=Utils.get_spark_session(job_run_env)
+
+    logger=Log4j(spark)
+    logger.warn("finished creating spark session")
